8b660662987996097184c4ba3aff1863adf2ae4a
bwauu <benjamin.williamsappelberg.0315@student.uu.se>
2022-05-02 12:13:45 +0200

    v.0

--- server/app.js
+++ server/app.js
@@ -2,12 +2,12 @@ const express = require('express'); // npm install express
 const { graphqlHTTP } = require('express-graphql'); // npm install express-graphql
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
-
+const MONGO_DB_PORT = null; // put desired port number; e.i. 27017
 const app = express();
-const DB_URL = "";
+const DB_URL = `mongodb://localhost:${MONGO_DB_PORT}`;
 mongoose.connect(DB_URL)
 mongoose.connection.once('open', () => {
-  console.log('connected to database');
+  console.log('Program sucessfully opened a connection to MongoDB server');
 }) 
 
 const PORT = 300;
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,9 @@
-const graphql = require('graphql');
+const graphql = require("graphql"); // GraphQL is a query language for APIs and a runtime for fulfilling those queries with your ... https://www.oreilly.com/library/view/data-lake-for/9781787281349/5140e9d5-53d9-421b-a73b-ff988bd5c4d9.xhtml
+const _ = require("lodash");
+const Book = require("../models/book.js");
+const Author = require("../models/author.js")
+const mongoose = require("mongoose");
 // describe object type we want in our graph.
-// var port === 300;
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -9,106 +12,132 @@ const {
     GraphQLInt,
     GraphQLList
 } = graphql; // destruct GraphQLObjectType from lib grab different objects from lib
-
-const _ = require('lodash');
-
-// THIS IS SEMISTRUCTURED DATA SAVED
-var books = [
-    {name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'}, 
-    {name: 'Harry Potter and the Sorcery Stone', genre: 'Fantasy', id: '2', authorId: '2'},
-    {name: 'Frankenstein', genre: 'Sci-Fi', id: '3', authorId: '3'},
-    {name: 'Harry Potter and the Chamber of Secrets', genre: 'Fantasy', id: '4', authorId: '2'},
-    {name: 'Harry Potter and the Prisoner from Azkaban', genre: 'Fantasy', id: '5', authorId: '2'},
-    {name: 'Harry Potter and the Goblet of Fire', genre: 'Fantasy', id: '6', authorId: '2'}
-];
-
-var authors = [
-    {name: 'Patrick Rothfuss', age: 44, id: '1'},
-    {name: 'J.K Rowling && benjamin.williams-appelberg@payex.com', age: 81, id: '2'},
-    {name: 'Mary Shelley', age: 225, id: '3'}
-];
-
 // initialize our first GraphQLObject  // notice that both id of these object has a GraphQLID type which can be both a int and a string.
 const AuthorType = new GraphQLObjectType({
     name: 'Author',
     fields: () => ({
-        id: {type: GraphQLID},
-        name: {type: GraphQLString},
-        age: {type: GraphQLInt},
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
         books: {
             type: new GraphQLList(BookType),
-            resolve(parent,args) {
-                return _.filter(books,{authorId: parent.id}) // {authorId = 2  => can remain in the array} // will return and filter everything with passed arg id
+            resolve(parent, args) {
+                return _.filter(books, { authorId: parent.id }) // {authorId = 2  => can remain in the array} // will return and filter everything with passed arg id
             }
         }
-            
+
     })
 });
-
-// Identified our first GraphQLObjectType
+// Identified our second GraphQLObjectType
 // booktype has fields which is a function, different options
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLID},
-        name: { type: GraphQLString},
-        genre: { type: GraphQLString},
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
         author: {
             type: AuthorType, // this is the reason fields hash to have a function as value and not a single json object. you have to wrap fields in function
             resolve(parent, args) {
-                console.log('This is parent object attribute `name`: ' + parent.name);
-                console.log('Parent object is a JSON object see when I print parent in the resolve() method.');
-                console.log(parent);
-                return _.find(authors, {id: parent.authorId});
+                console.log('This is parent object attribute `name`: ' + parent.name + "working with resolve. \n this is parent: " + parent);
+                // return _.find(authors, {id: parent.authorId});
             }
         }
     })
 });
-
 // when someone query a book i want you to use this BookType. // argument i want you to use in this query is id
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType', // RootQueryn är basically hela uppbyggnaden av GraphQL, det är här språket börjar och all teknik man behöver förstå, hur dessa querys arbetar och byter ut info + dirigerar delegater/andra funcs.
     fields: {
         book: {
             type: BookType,
-            args: {id: {type: GraphQLID}},
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                console.log('Program was called from GraphiQL-UI @http://localhost:port/graphql')
                 console.log("I am microservice and a middleware section sending users resources of GraphQLObject: " + BookType.toString() + " && ID: " + args.id);
-                return _.find(books, {id: args.id});
+                // return _.find(books, {id: args.id});
             }
         },
         author: {
-            type: AuthorType,
-            args: { id: { type: GraphQLID }},
+            "type": AuthorType,
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                console.log('Program was called from GraphiQL-UI @http://localhost:port/graphql')
                 console.log("I am microservice and a middleware section sending users resources of GraphQLObject: " + AuthorType.toString() + " && ID: " + args.id);
-                return _.find(authors, {id: args.id});
+                // return _.find(authors, {id: args.id});
             }
         },
-
-        ericsFavoritBocker: {
+        books: {
             type: new GraphQLList(BookType),
             // will not need args key and it values, cuz we want to return a booklist. we don't want to search for a particular book
             resolve(parent, args) {
-                return books;
+                // return books;
             }
         },
-
-        benjaminsFavoritFortfattare: {
+        authors: {
             type: new GraphQLList(AuthorType),
-            resolve(parent,args) {
-                return authors;
+            resolve(parent, args) {
+                // return authors;
             }
-
         }
-         
     }
 });
 
+const Mutation = new GraphQLObjectType({
+    "name": "Mutation",
+    "fields": {
+        /* args.name: "Benjamin_Appelberg", args.age: 250 */
+        /* => mutation{addAuthor(name, age){name age}} */
+        "addAuthor": {
+            "type": AuthorType,
+            "args": {
+                "name": { type: GraphQLString },
+                "age": { type: GraphQLInt }
+            },
+            // resolve func takes user args and store it in db
+            resolve(parent, args) {
+                console.log(`resolve function called with parent: ${parent} args: ${args.name + args.age}`);
+                try {
+
+
+                    // imported Author model exported from author.js see const assign in the start of schema.js.
+                    let author = new Author({
+                        "name": args.name,
+                        "age": args.age
+                    });
+                    author.save(); // saves author obj to mongodb
+                } catch (e) {
+                    console.log(e + "status code: 400");
+                }
+            } // end of resolve func 
+        }, // end of addAuthor object
+        "addBook": {
+            "type": BookType,
+            "args": {
+                "name": { "type": GraphQLString },
+                "genre": { "type": GraphQLString },
+                "authorId": { "type": GraphQLID }
+            },
+            resolve(parent, args) {
+                try {
+                    let book = new Book({
+                        "name": args.name,
+                        "genre": args.genre,
+                        "authorId": args.authorId
+                    });
+                    book.save();
+                    console.log("Sucessfully saved dataobject:" + book);
+                }
+                catch (e) {
+                    console.log(e + "status code: 400");
+                }
+            }
+        }
+
+    }
+}); // end of Mutation GraphQLObjectType
+
 // This is the query that is interacting with front end user => get exports to app.js
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation // mutation object that helps us önsvärt mutera data.
 });
 
